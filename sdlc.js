33eb1a2dd7f08ffc3a21817149b78a2e72215c3c
Hannah Song <hannahsong89@gmail.com>
2022-11-20 21:52:24 -0600

    second commit

--- sdlc.js
+++ sdlc.js
@@ -25,3 +25,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "Analysis of Requirements is the second step, this is where we discuss what we're going to build and what we are trying to deliver."
+const design = "Design is the third step, this is when we decide what it's going to look like, how is it going to feel, etc."
+const implementation = "Implementation is the fourth step, this is where developers apply their process and work into creating their product"
+const testingAndIntegration = "Testing and Integration is the fifth step, this is when the developers pass the product on to the QAs where they will test the product to ensure that it is running as designed."
+const maintain = "Maintain is the final step, this is when team will continuously change, modify, and update the product to keep up with the customer's needs."
